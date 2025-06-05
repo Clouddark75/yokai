3b85b793d7171ae0fa2a3c6a142f841c62c049d6
Clouddark75 <84534855+Clouddark75@users.noreply.github.com>
2025-06-05 10:10:23 -0400

    Update Downloader.kt

--- app/src/main/java/eu/kanade/tachiyomi/data/download/Downloader.kt
+++ app/src/main/java/eu/kanade/tachiyomi/data/download/Downloader.kt
@@ -373,9 +373,9 @@ class Downloader(
             }
 
             // Start downloading images, consider we can have downloaded images already
-            // Concurrently do 2 pages at a time
+            // Concurrently do 6 pages at a time
             pageList.asFlow()
-                .flatMapMerge(concurrency = 2) { page ->
+                .flatMapMerge(concurrency = 6) { page ->
                     flow {
                         withIOContext { getOrDownloadImage(page, download, tmpDir) }
                         emit(page)
